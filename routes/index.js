45a6e8a9a6613a2e522e09cb485726ff6cea96d7
Kingfree <anytjf@gmail.com>
2016-12-28 11:53:21 +0800

    换用express

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '闹萌' });
+});
+
+module.exports = router;
