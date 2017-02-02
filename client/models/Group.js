3e2b3feb421d82340b3789aaf03b7fe4213e5ffb
Kingfree <anytjf@gmail.com>
2017-02-03 00:23:17 +0800

    换用Laravel

--- client/models/Group.js
+++
@@ -1,7 +0,0 @@
-module.exports = function Group() {
-  return {
-    title: '分组1',
-    choice: 1,
-    characters: []
-  }
-}
--- routes.js
+++
@@ -1,7 +0,0 @@
-var controllers = require('./controllers');
-
-var routes = [].concat(
-  controllers.character
-);
-
-module.exports = routes;
