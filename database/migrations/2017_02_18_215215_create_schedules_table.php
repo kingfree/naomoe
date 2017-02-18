ab3bd3809d5023a3846db7c834901fbbd5e5050c
kingfree <kingfree@toyama.moe>
2017-02-18 21:57:15 +0800

    引入日程表

---
+++ database/migrations/2017_02_18_215215_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('year')->unsigend();
+            $table->integer('month')->unsigend();
+            $table->integer('day')->unsigend();
+            $table->integer('competition_id')->unsigend();
+            $table->foreign('competition_id')->references('id')->on('competitions')->onDelete('cascade');
+            $table->boolean('visible')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
