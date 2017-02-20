1e84a294b13c0dd864285a4c7563157e4ed8e938
kingfree <kingfree@toyama.moe>
2017-02-20 11:57:50 +0800

    redirect

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -14,7 +14,7 @@ class ScheduleController extends Controller
         $today = Carbon::today();
         $cal = Schedule::where('year', $today->year)->where('month', $today->month)->where('day', $today->day)->first();
         if ($cal and $cal->visible) {
-            return route('after', ['id' => $cal->competition_id]);
+            return redirect()->route('after', ['id' => $cal->competition_id]);
         }
         $page = Page::find(1);
         if (!$page) $page = new Page;
