d6794ddeef70f592eacd04d28443bc8a96ddbc53
kingfree <kingfree@toyama.moe>
2017-02-25 11:07:18 +0800

    别这样

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (starts_with(env('APP_URL'), 'https://')) {
-            URL::forceSchema('https');
-        }
     }
 
     /**
